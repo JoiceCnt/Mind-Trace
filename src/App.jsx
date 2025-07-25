9ddd5fa3e3d8e26ab6e4bb7ef05c4611eb336c6e
Joice Conte <joiceconte@gmail.com>
2025-07-25 10:28:20 +0200

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import "./App.css";
+import { useState } from "react";
+import "./index.css";
+import Calendar from "react-calendar";
+
+import "react-calendar/dist/Calendar.css";
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <>
-      <div></div>
+      <div>
+        <h2>Calendar</h2>
+        <Calendar onChange={setSelectedDate} value={selectedDate} />
+        <p>Daily Secctions {selectedDate.toDateString()}</p>
+      </div>
     </>
   );
 }
