bd481a504b5e724c54f252b90c7493cee9340516
Noemí Mira <noemi.miranavarro@gmail.com>
2025-07-26 17:12:24 +0200

    EmotionSelectorPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./index.css";
 import Calendar from "react-calendar";
+import { Routes, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import "react-calendar/dist/Calendar.css";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import EmotionSelectorPage from "./Pages/EmotionSelectorPage";
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(new Date());
@@ -10,9 +15,32 @@ function App() {
   return (
     <>
       <div>
+        <Navbar/>
+
         <h2>Calendar</h2>
         <Calendar onChange={setSelectedDate} value={selectedDate} />
         <p>Daily Secctions {selectedDate.toDateString()}</p>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <h1>Select the option below to login:</h1>
+                <Link to="/emotion-selector">
+                  <button>Patient</button>
+                </Link>      
+              </div>
+            }
+          />
+          <Route path="/emotion-selector" element={<EmotionSelectorPage/>} />
+
+
+        </Routes>
+
+
+
+        <Footer/>
       </div>
     </>
   );
