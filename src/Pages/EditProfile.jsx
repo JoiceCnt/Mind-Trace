fe06b56770e5020f5fbc57198a3870d23f99e989
Noemí Mira <noemi.miranavarro@gmail.com>
2025-08-01 10:42:11 +0200

    edit profile

--- src/Pages/EditProfile.jsx
+++ src/Pages/EditProfile.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "../Styles/EditProfile.css";
 
 function EditProfile() {
   const [profile, setProfile] = useState({ name: "", email: "", birthday: "" });
